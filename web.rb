eb5b483be15a23ec878c368ac28e554eafa750b3
baydet <evsyuchenya@targetprocess.com>
2018-04-18 13:20:12 +0300

    test

--- web.rb
+++ web.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
+require 'rubygems'
 
 get '/' do
   "Hello, world"
+end
+
+get '/apple-app-site-association' do
+  File.read(File.join('public', 'apple-app-site-association'))  
 end
